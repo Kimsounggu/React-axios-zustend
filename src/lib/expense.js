a00130ded67a7e8daf4ecf9626f58a2e8d012545
Kimsounggu <tjdrn4657@naver.com>
2024-06-14 12:00:50 +0900

    update: json-server

--- src/lib/expense.js
+++ src/lib/expense.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const JSON_SERVER_HOST = "https://abyssinian-watery-angolatitan.glitch.me/";
+const JSON_SERVER_HOST = "https://melodious-rich-pick.glitch.me";
 
 export const getExpenses = async () => {
   try {
